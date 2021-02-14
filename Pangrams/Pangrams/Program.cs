eea269abd91515af380c57b5121d4e0a0123234d
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-02-14 20:41:34 +0600

    Add files via upload

---
+++ Pangrams/Pangrams/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static String IsPangrams(String line)
+        {
+            String result = "pangram";
+            bool[] status = new bool[26];
+            Char[] stc = line.ToCharArray();
+            int len = stc.Length;
+            for(int i = 0; i < len; i++)
+            {
+                if (Char.IsLower(stc[i]))
+                {
+                    status[stc[i] - 'a'] = true;
+                }
+                else if (Char.IsUpper(stc[i]))
+                {
+                    status[stc[i] - 'A'] = true;
+                }
+            }
+            for(int i = 0; i <26; i++)
+            {
+                if(status[i] == false)
+                {
+                    result = "not pangram";
+                    break;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            Console.WriteLine(IsPangrams(s));
+        }
+    }
+}
