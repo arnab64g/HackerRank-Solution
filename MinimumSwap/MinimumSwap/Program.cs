ff3d2c4c948620f08c16cbdd8a72a1ef56ad8d96
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-02-14 20:49:47 +0600

    Add files via upload

---
+++ MinimumSwap/MinimumSwap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MinimumSwap
+{
+    class Program
+    {
+        static int MinimumSw(int[] a)
+        {
+            int n = 0;
+            int len = a.Length;
+            int swloc, temp; 
+            for(int i = 0; i < len; i++)
+            {
+                while(a[i] != i + 1)
+                {
+                    n++;
+                    swloc = a[i] - 1;
+                    temp = a[swloc];
+                    a[swloc] = a[i];
+                    a[i] = temp;
+                }
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            String[] s = Console.ReadLine().Split(' ');
+            int len = s.Length;
+            int[] num = new int[len];
+            for(int i = 0; i < len; i++)
+            {
+                num[i] = Int32.Parse(s[i]);
+            }
+            Console.WriteLine(MinimumSw(num));
+        }
+    }
+}
---
+++ WiredNumber/WiredNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WiredNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string v = Console.ReadLine();
+            int input = Convert.ToInt32(v);
+            //input = Convert.ToInt32(rs);
+            
+            if(input % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (input %2 == 0) {
+                if(input >= 2 && input <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (input >= 6 && input <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (input % 2 == 0 && input > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (input % 2 != 0)
+                {
+                    Console.WriteLine("Weird");
+                }
+            }
+            
+        }
+    }
+}
