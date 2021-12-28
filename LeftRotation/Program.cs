904d5e9413c500876718ad94f894cfd5abf24306
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-12-28 21:43:03 +0600

    Add files via upload

---
+++ LeftRotation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeftRotation
+{
+    class Program
+    {
+        public static void Main(string[] args){
+            
+            string[] nm = Console.ReadLine().Split(' ');
+            int n = int.Parse(nm[0]);
+            int m = int.Parse(nm[1]);
+            string[] value = Console.ReadLine().Split(' ');
+            for (int i = m; i < n; i++)
+            {
+                Console.Write(value[i] + " ");
+            }
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write(value[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
