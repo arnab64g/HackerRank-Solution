c9a098ff224676bf70090fef08971c207df15ce2
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-12-30 20:39:30 +0600

    Add files via upload

---
+++ SparseArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main(String[] args){
+        int n = Int32.Parse(Console.ReadLine());
+        Dictionary<string, int> dt = new Dictionary<string, int>();
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            if(dt.ContainsKey(s)){
+                dt[s]++;
+            }
+            else dt.Add(s, 1);
+            
+        }
+        int m = int.Parse(Console.ReadLine());
+        for (int i = 0; i < m; i++)
+        {
+            string s = Console.ReadLine();
+            if (dt.ContainsKey(s))
+            {
+                Console.WriteLine(dt[s]);
+            }
+            else Console.WriteLine(0);
+        }
+    }
+}
